495993e915cffe13531c6aae8e486d1064edffcb
Francisco Sanchez Oliver <FSanchezO@axpenet.local>
2023-12-17 07:16:40 +0100

    easy motion activated and changed leader key

---
+++ main.py
@@ -0,0 +1,12 @@
+def sum_two_numbers(a, b):
+    return a + b
+
+def main():
+    num1 = 5
+    num2 = 7
+    result = sum_two_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is: {result}")
+
+if __name__ == "__main__":
+    main()
+    print("hola")
